f4064f92428f283e3a0280ac8ffd288608092bf4
michael <loganmitchellahaha@gmail.com>
2023-02-22 00:26:00 +0200

    Using Classes

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ function OffsetOfdeadLineAndCurrentTime(endOfTime) { // оприділяє рі�
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 10000);
+    // const modalTimerId = setTimeout(openModal, 10000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -152,4 +152,71 @@ function OffsetOfdeadLineAndCurrentTime(endOfTime) { // оприділяє рі�
     }
 
     window.addEventListener('scroll', showModalByScroll);
-});
+
+        //-----------CLASS-------------
+
+        class MenuCard {
+           constructor(src, alt, subtitle, descr, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);// в властивості his.parent лежить DOМ element
+            this.convert = 40; // курс по якому буде долар конвертиртись в гривні 
+            this.convertToUAH(); // коли всі властивості виконаються по порядку, виконається метод
+           } 
+
+           convertToUAH(){
+            this.price *= this.convert; 
+           }
+
+           redner(){
+            const divElement = document.createElement("div");
+            divElement.innerHTML = 
+                ` 
+                    <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                 `;
+                this.parent.append(divElement);
+           }
+        }
+
+
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy", 
+            'Меню "Фитнес" ',
+            "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+            6.5,
+            ".menu .container"
+        ).redner(); 
+
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite", 
+            ' Меню "Премиум" ',
+            "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+            10,
+            ".menu .container"
+        ).redner(); 
+
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post", 
+            ' Меню "Постное" ',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков',
+            5,
+            ".menu .container"
+        ).redner(); 
+
+
+    
+    });
